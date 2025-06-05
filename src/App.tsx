8dde72c42083a6276dedbadc82d680c318c905d7
laszloekovacs <laszloekovacs@gmail.com>
2025-06-05 14:08:47 +0200

    add some flare

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import ModeSelectScreen from './ModeSelectScreen'
 import Controller from './Controller'
 import Player from './Player'
+import "./App.css"
 
 export type Modes = "player" | "controller"
 
@@ -16,7 +17,26 @@ function App() {
     return <Controller />
   }
 
-  return <ModeSelectScreen onSelect={setRole} />
+  return (
+    <div>
+      <ModeSelectScreen onSelect={setRole} />
+      <div className="scrolling-text">
+        <span>🐝 Buzzing with creativity! 🐝 Buzzing with creativity! 🐝</span>
+      </div>
+      <div className="wrapper">
+        <div className="marquee">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat, ante eu bibendum tincidunt, sem lacus vehicula augue, ut suscipit.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat, ante eu bibendum tincidunt, sem lacus vehicula augue, ut suscipit.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+
+
 }
 
 export default App
