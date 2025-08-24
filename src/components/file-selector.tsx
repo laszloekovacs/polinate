eeaf4687a7a3cec3cd30f2fa52fc3660e450a228
laszloekovacs <laszloekovacs@gmail.com>
2025-08-24 18:21:40 +0200

    removed comments

--- src/components/file-selector.tsx
+++ src/components/file-selector.tsx
@@ -1,21 +1,18 @@
-import { z } from "zod"
+import { useEffect, useRef, type ChangeEvent } from "react"
 
 
-export default function FileSelector({ ref }: { ref: React.Ref<HTMLInputElement> }) {
-    // state when file is selected
-    /*
-        const fileSelectSchema = z.object({
-            current: z.instanceof(HTMLInputElement)
-        })
-    
-        const fileSelectRef = fileSelectSchema.parse(ref)
-    
-        console.log(fileSelectRef.current.files)
-    */
+export default function FileSelector() {
+    const ref = useRef<HTMLInputElement>(null)
+
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(ref.current?.files?.[0])
+    }
+
 
     return (
         <label className="file-selector-label">
-            <input accept="video/*" ref={ref} className="file-selector" name="fileinput" type="file" />
+            <input accept="video/*" ref={ref} className="file-selector" name="fileinput" type="file" onChange={handleChange} />
             <div className="file-selector-content">
                 <div>
                     <p>Drag and drop a video file or click and select</p>
@@ -24,3 +21,5 @@ export default function FileSelector({ ref }: { ref: React.Ref<HTMLInputElement>
         </label>
     )
 }
+
+
